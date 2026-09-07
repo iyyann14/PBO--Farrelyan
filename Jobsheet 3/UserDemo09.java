6ba64661a60397190c7472d412dfdebfea01db90
iyyann14 <iniiyyann@gmail.com>
2026-09-07 09:42:29 +0700

    Percobaan 1

---
+++ Jobsheet 3/UserDemo09.java
@@ -0,0 +1,6 @@
+public class UserDemo09 {
+    public static void main(String[] args) {
+        User09 user1 = new User09("Farrelyan", "farrelyan@example.com");
+        user1.displayInfo();
+    }
+}
